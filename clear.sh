75d7941bfb86b3e98a34edf1afad334a04e07bb7
Nisham <moonblade168@gmail.com>
2016-12-10 15:01:52 +0530

    updates

---
+++ clear.sh
@@ -0,0 +1,5 @@
+#clear all created files
+rm emptyTables
+rm noContraints.sql
+rm tableCreation.sql
+rm wordCountInfo
--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,7 @@
+sqlFile=ddfs_Fri.sql
+
 #get only create tables
-sed -n -e '/CREATE TABLE/,/;/p' ddfs_Fri.sql > tableCreation.sql
+sed -n -e '/CREATE TABLE/,/;/p' $sqlFile > tableCreation.sql
 
 #remove constraints in the tables
 sed '/CONSTRAINT/d' tableCreation.sql > noContraints.sql
@@ -11,24 +13,30 @@ sed -i 's/^) ENGINE.*;/);/g' noContraints.sql
 #make a file with just tableNames
 cat tableCreation.sql | grep "CREATE TABLE" | cut -d' ' -f3 > tableNames
 
-# make tables folder
+# # make tables folder
 # mkdir tables
 
-#get each individual tables in separate file
-# for f in $(cat tableNames); do sed -n -e "/LOCK TABLES $f/,/UNLOCK TABLES/p" ddfs_Fri.sql > tables/$f ;echo $f; done
-# rename the files to better names (remove surrounding ` and append .sql)
-for f in *; do mv $f ${f%\`}; done
-for f in *; do mv $f ${f/\`}; done
-for f in *; do mv $f $f.sql; done
-
+# #get each individual tables in separate file
+# for f in $(cat tableNames); do sed -n -e "/LOCK TABLES $f/,/UNLOCK TABLES/p" $sqlFile > tables/$f ;echo $f; done
+# # rename the files to better names (remove surrounding ` and append .sql)
+# cd tables
+# for f in *; do mv $f ${f%\`}; done
+# for f in *; do mv $f ${f/\`}; done
+# for f in *; do mv $f $f.sql; done
+# cd ..
 
 
 #get word count information
 wc tables/* > wordCountInfo
 
-#find which all tables are empty, ie have no content
+#find which all tables are empty, ie have no content (check for ones with only 4 lines)
 wc -l tables/* | grep "\b4\b" | cut -f11 -d' ' > emptyTables
 
 
 #remove those tables
 for f in $(cat emptyTables); do rm $f; done
+
+# # Separate inserted values into single lines
+cd tables
+for f in *.sql; do echo $f; cat $f | grep -hoP "\(.*?\)[,;]" > ../values/${f%.sql}; done
+cd ..
